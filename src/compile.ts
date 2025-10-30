c5d898617766a6ad5fddba5073971a2fa212d0a8
raynecloudy <contact@raynec.dev>
2025-10-30 06:28:13 -0700

    project creation utility, compiler work

--- src/compile.ts
+++ src/compile.ts
@@ -1,14 +1,33 @@
 #!/usr/bin/env node
 import chalk from "chalk";
-import { constants } from "node:fs"; 
+import { constants, PathLike } from "node:fs"; 
 import { cwd, env, exit } from "node:process";
-import { access, mkdir, readFile, rm } from "node:fs/promises";
+import { access, mkdir, readFile, rm, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 import { program } from "commander";
 
 type WunderbarConfig = {
   output: string
 };
+type npmPackageConfig = {
+  name: string,
+  description: string,
+  version: string,
+  license: string,
+  author: string
+};
+
+const wd = cwd();
+
+const exists = async (path: PathLike) => {
+  try {
+    await access(path, constants.F_OK);
+    return true;
+  } catch (error) {
+    if (error.code === "ENOENT") return false;
+    else throw error;
+  }
+}
 
 program.option("-d --debug", "display debug information", false);
 program.option("-v --version", "display version", false);
@@ -26,24 +45,40 @@ const cError = console.error;
 console.debug = (...data: any[]) => logDebug ? cError(chalk.yellow.bold("(debug)"), ...data) : void(0);
 console.error = (...data: any[]) => cDebug(chalk.red.bold("(error)"), ...data);
 
-try {
-  await access(join(cwd(), "wbconfig.json"), constants.F_OK);
-} catch (error) {
-  if (error.code === "ENOENT") {
-    console.error("file wbconfig.json does not exist or cannot be accessed.");
-  } else throw error;
+if (!await exists(join(wd, "wbconfig.json"))) {
+  console.error("file wbconfig.json does not exist or cannot be accessed.");
+  exit(1);
 }
 
-const wbconfig: Partial<WunderbarConfig> = JSON.parse(await readFile(join(cwd(), "wbconfig.json"), "utf-8"));
+const wbconfig: Partial<WunderbarConfig> = JSON.parse(await readFile(join(wd, "wbconfig.json"), "utf-8"));
+const buildDir = join(wd, wbconfig.output ?? "build");
 
-if (join(cwd()) === join(cwd(), wbconfig.output ?? "build")) {
+if (join(wd) === buildDir) {
   console.error("output directory cannot be working directory");
+  exit(1);
 }
 
-await rm(join(cwd(), wbconfig.output ?? "build"), {
+await rm(buildDir, {
   recursive: true,
   force: true
 })
-await mkdir(join(cwd(), wbconfig.output ?? "build"), {
+await mkdir(buildDir, {
   recursive: true
 });
+
+await writeFile(join(buildDir, ".gitignore"), "*");
+
+const packageJSON: npmPackageConfig = JSON.parse(await readFile(join(wd, "package.json"), "utf-8"));
+
+if (await exists(join(wd, "server/"))) {
+  await writeFile(join(buildDir, "package.json"), JSON.stringify({
+    name: packageJSON.name,
+    description: packageJSON.description,
+    version: packageJSON.version,
+    main: "server.js",
+    license: packageJSON.license,
+    author: packageJSON.author,
+    type: "module"
+  }));
+  await writeFile(join(buildDir, "server.js"), "\n");
+}
---
+++ src/create.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import { createInterface } from "readline/promises";
+import { cwd, stdin, stdout } from "process";
+import { join } from "path";
+import { mkdir, writeFile } from "fs/promises";
+import { valid } from "semver";
+
+const wd = cwd();
+
+const i = createInterface({
+  input: stdin,
+  output: stdout,
+});
+let name = await i.question("project name: ");
+name = name.length > 0 ? name : "wunderbar-project";
+let description = await i.question("description: ");
+description = description.length > 0 ? description : undefined
+let version = await i.question("version (0.0.1): ");
+version = version.length > 0 && valid(version) ? version : "0.0.1";
+let license = await i.question("license (ISC): ");
+license = license.length > 0 ? license : "ISC";
+let author = await i.question("author: ");
+author = author.length > 0 ? author : undefined
+i.close();
+
+await mkdir(join(wd, name), {
+  recursive: true
+});
+await writeFile(join(wd, name, "package.json"), JSON.stringify({
+  name,
+  description,
+  version,
+  license,
+  author
+}));
+await writeFile(join(wd, name, "wbconfig.json"), JSON.stringify({
+  output: "build"
+}));
