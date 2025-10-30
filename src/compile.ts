595197ac8d51087cbcb86109c693047c3e3d12b5
raynecloudy <contact@raynec.dev>
2025-10-29 20:41:26 -0700

    wbconfig.json checking

--- src/compile.ts
+++ src/compile.ts
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
+import { constants } from "node:fs"; 
+import { cwd } from "node:process";
+import { access } from "node:fs/promises";
+import { join } from "node:path";
 import { program } from "commander";
 
 program.option("-d --debug", "display debug information", false);
 program.parse();
 
 const logDebug: boolean = program.getOptionValue("debug");
+
+try {
+  await access(join(cwd(), "wbconfig.json"), constants.F_OK)
+} catch (error) {
+  if (error.code === "ENOENT") {
+    console.error("file wbconfig.json does not exist or cannot be accessed.")
+  }
+  else throw error;
+}
